42cc3f9aea6a9204960fa8b05044189d8dc082db
Mohit <mohitpandey126@gmail.com>
2023-04-17 22:45:02 +0530

    Changes done and code clean up

--- fir/telbott.py
+++ fir/telbott.py
@@ -30,26 +30,27 @@ def inventory(message):
     
     bot.send_message(message.chat.id,"Available items ")
     inventory=[
-    {'name': 'maggie', 'price': 10, 'quantity': 100, 'weight': 0.5, 'type': 'Type 1', 'brand': 'Brand 1'},
-    {'name': 'chips', 'price': 10, 'quantity': 110, 'weight': 1.0, 'type': 'Type 2', 'brand': 'Brand 2'},
-    {'name': 'biscuits', 'price': 15, 'quantity': 120, 'weight': 1.0, 'type': 'Type 3', 'brand': 'Brand 3'},
-    {'name':'rice','price':30,'quantity':100,'weight':50},
-    {'name':'dal','price':25,'quantity':100,'weight':9.0},
-    {'name':'Apple','price':80,'quantity':100,'weight':30},
-    {'name':'Garam masala','price':10,'quantity':150,'weight':10},
-    {'name':'sugar','price':30,'quantity':50,'weight':20}
+    {'name': 'maggie', 'price': 10, 'quantity': 100, 'weight': 0.5, 'brand':'yepiee'},
+    {'name': 'chips', 'price': 10, 'quantity': 110, 'weight': 1.0, 'brand':'Bingo'},
+    {'name': 'biscuits', 'price': 15, 'quantity': 120, 'weight': 1.0, 'brand':'Britannia'},
+    {'name':'rice','price':30,'quantity':100,'weight':50,'brand':'Bullet'},
+    {'name':'dal','price':25,'quantity':100,'weight':9.0,'brand':'Cjh'},
+    {'name':'Apple','price':80,'quantity':100,'weight':30,'brand':'apple'},
+    {'name':'Garammasala','price':10,'quantity':90,'weight':10,'brand':'aachi'},
+    {'name':'sugar','price':30,'quantity':50,'weight':20,'brand':'Madhur'},
+    {'name':'kurkure','price':10,'quantity':90,'weight':0.9,'brand': 'Takatak'}
     
     ]
     print(inventory)
     
-    bot.send_message(message.chat.id, "item  price  quantity  weight")
+    bot.send_message(message.chat.id, "Item  Price  Quantity  Weight  Brand")
     for item in inventory:
         
-        # items=""
-        items+= f"{item['name']} - {item['price']}Rs - {item['quantity']}left - {item['weight']} \n"
+        items=""
+        items+= f"{item['name']} - {item['price']}Rs - {item['quantity']}left - {item['weight']} -{item['brand']}\n"
         # items+=item.name
       
-        print(items)
+        
         # bot.send_message(message.chat.id, "item  price  quantity  weight")
         bot.send_message(message.chat.id, items)
 @bot.message_handler(['order'])
@@ -60,7 +61,7 @@ def order(message):
 def orders_step(message):
     global ordd
     ordd=message.text
-    inventory_items=['rice','dal','maggie','biscuit','sugar','Apple','Garam masala','chips']
+    inventory_items=['rice','dal','maggie','biscuit','sugar','Apple','Garam masala','chips','kurkure']
     
     if ordd in inventory_items:
         bot.send_message(message.chat.id,"great this item is availble!!")
@@ -74,7 +75,7 @@ def quant_step(message):
     global qu
     qu=message.text
     
-    if int(qu) <100:
+    if int(qu) <=100:
         bot.send_message(message.chat.id,"ok done!!!!")
         ca=bot.send_message(message.chat.id,"Would you like to add this to cart and Checkout!!")
         bot.register_next_step_handler(ca,carts_step)
@@ -88,7 +89,7 @@ def carts_step(message):
     ans=message.text
     if ans=='yes'or'YES' or 'Yes':
         caart.update({ordd:qu})
-        dict={'rice':20,'dal':10,'maggie':10,'sugar':30,'Apple':80,'chips':10,'biscuits':15,'Garam masala':10}
+        dict={'rice':20,'dal':10,'maggie':10,'sugar':30,'Apple':80,'chips':10,'biscuits':15,'Garam masala':10,'kurkure':10}
         for k in dict:
             if k ==ordd:
                 s=dict[ordd]
@@ -167,134 +168,4 @@ def Jobs_opening(message):
         
     
         
-        
-
-    
-    
-#     print(items)
-# def inventory_handler(update,context):
-#     message="inventory item\n item price quantity weight\n"
-#     for item in inventory:
-#          message += f"{item['name']} - ${item['price']} - {item['quantity']}left - {item['weight']} \n"
-         
-#     update.message.reply_text(message)
-        
-# def content(update,context):
-#     update.message.reply_text("rice 20rs , dal 30rs , sweet 50rs, biscuit 10rs")
-# def contact(update,context):
-#     update.message.reply_text("you can contact us on @riyastore.com for any quries")
-# def handle_message(update,context):
-#     update.message.reply_text("you saide{update.message.text}")
-# def order_handler(update, context):
-#     context.user_data['order'] = []
-#     update.message.reply_text(text='What would you like to order?')
-#     item_name = update.message.text.lower()
-#     print(item_name)
-# def add_item_handler(update, context):
-#     update.message.reply_text("what u want?")
-#     item_name = update.message.text.lower()
-#     print(item_name)
-#     if item_name in inventory:
-#         context.bot.send_message( chat_id=update.effective_chat.id,text=f"{item_name} added to your order")
-            
-#     else:
-#          context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, that item is not available")
-            
-
-
-
-    
-    
-# updater=Updater('6051146126:AAHDjXqH5rF--9ub5uXmy7oEHWSUH7Dss5c', use_context=True)
-# disp=updater.dispatcher
-# disp.add_handler(CommandHandler("start",start))
-# disp.add_handler(CommandHandler("help",help))
-# disp.add_handler(CommandHandler("item",content))
-# disp.add_handler(CommandHandler("contact",contact))
-# disp.add_handler(CommandHandler("inventory",inventory_handler))
-# disp.add_handler(CommandHandler("order",order_handler))
-# disp.add_handler(CommandHandler("add",add_item_handler))
-# disp.add_handler(MessageHandler(Filters.text, add_item_handler))
- 
-
-
-# import telegram
-# from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-
-# # Define the inventory items and their attributes
-# inventory = [
-#     {'name': 'Product 1', 'price': 10, 'quantity': 100, 'weight': 0.5, 'type': 'Type 1', 'brand': 'Brand 1'},
-#     {'name': 'Product 2', 'price': 20, 'quantity': 50, 'weight': 1.0, 'type': 'Type 2', 'brand': 'Brand 2'},
-#     {'name': 'Product 3', 'price': 15, 'quantity': 80, 'weight': 0.8, 'type': 'Type 3', 'brand': 'Brand 3'},
-#     # Add more inventory items as necessary
-# ]
-
-# # Define the function to display the inventory items
-# def inventory_handler(update, context):
-#     message = 'Inventory Items:\n'
-#     for item in inventory:
-#         message += f"{item['name']} - ${item['price']} - {item['quantity']} left\n"
-#     context.bot.send_message(chat_id=update.effective_chat.id, text=message)
-
-# # Define the function to initiate the order process
-# def order_handler(update, context):
-#     context.user_data['order'] = []
-#     context.bot.send_message(chat_id=update.effective_chat.id, text='What would you like to order?')
-
-# # Define the function to add an item to the order
-# def add_item_handler(update, context):
-#     item_name = update.message.text
-#     for item in inventory:
-#         if item['name'] == item_name:
-#             context.user_data['order'].append(item)
-#             context.bot.send_message(chat_id=update.effective_chat.id, text=f"{item_name} added to your order")
-#             break
-#     else:
-#         context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, that item is not available")
-
-# # Define the function to display the final order confirmation message
-# def confirm_order_handler(update, context):
-#     message = 'Order:\n'
-#     total_cost = 0
-#     for item in context.user_data['order']:
-#         message += f"{item['name']} - ${item['price']}\n"
-#         total_cost += item['price']
-#     message += f"Total Cost: ${total_cost}"
-#     context.bot.send_message(chat_id=update.effective_chat.id, text=message)
-
-# # Define the function to send a message to the store owner
-# def send_order_handler(update, context):
-#     message = 'New Order:\n'
-#     total_cost = 0
-#     for item in context.user_data['order']:
-#         message += f"{item['name']} - ${item['price']}\n"
-#         total_cost += item['price']
-#     message += f"Total Cost: ${total_cost}"
-#     context.bot.send_message(chat_id=STORE_OWNER_CHAT_ID, text=message)
-
-# # Define the function to display the order status
-# def status_handler(update, context):
-#     # Implement the logic to retrieve the order status from the store owner or a database
-#     order_status = "Order Placed"
-#     context.bot.send_message(chat_id=update.effective_chat.id, text=order_status)
-
-# # Set up the bot with the handlers
-# TOKEN = '6051146126:AAHDjXqH5rF--9ub5uXmy7oEHWSUH7Dss5c'
-# STORE_OWNER_CHAT_ID = 'your_store_owner_chat_id_here'
-# updater = Updater(token=TOKEN, use_context=True)
-# dispatcher = updater.dispatcher
-# dispatcher.add_handler(CommandHandler('inventory', inventory_handler))
-# dispatcher.add_handler(CommandHandler('order', order_handler))
-
-
-
-# updater.start_polling()
-# updater.idle()
-
-
-
-
-
-
-
 bot.polling()
